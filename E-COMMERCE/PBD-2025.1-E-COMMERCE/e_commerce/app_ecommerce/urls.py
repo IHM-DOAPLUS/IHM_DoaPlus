69ae6d5153c72ce6417a1b42e13a4984beb4221b
Jose Ramon Alves Frazao <ramonalves1994@gmail.com>
2025-05-18 18:49:02 -0300

    update
    tela inicial, login, dashboard, tela de produto, tela de cadastrar user

--- E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/app_ecommerce/urls.py
+++ E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/app_ecommerce/urls.py
@@ -5,5 +5,16 @@ from . import views
 app_name = 'ecommerce'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('portifolio', views.portifolio, name='portifolio'),
+    path('starter', views.starter_page, name='starter'),
+    path('service', views.service, name='service'),
+    path('dash', views.dash, name='dash'),
+    path('user', views.user, name='user'),
+
+
+
+
+
+
 ]
--- E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/app_ecommerce/views.py
+++ E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/app_ecommerce/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
+
+
+def portifolio(request):
+    return render(request, 'portfolio-details.html')
+
+
+def starter_page(request):
+    return render(request, 'starter-page.html')
+
+
+def service(request):
+    return render(request, 'service-details.html')
+
+def dash(request):
+    return render(request, 'dash.html')
+
+def user(request):
+    return render(request, 'user.html')
