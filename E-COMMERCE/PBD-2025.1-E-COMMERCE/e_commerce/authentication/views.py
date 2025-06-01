4e000242f61fb62e606e7348578559cac6d6d95c
Jose Ramon Alves Frazao <ramonalves1994@gmail.com>
2025-06-01 15:59:45 -0300

    update

--- E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/authentication/views.py
+++ E-COMMERCE/PBD-2025.1-E-COMMERCE/e_commerce/authentication/views.py
@@ -9,19 +9,23 @@ from django.contrib.auth.decorators import login_required
 
 
 def user_page(request):
-    form = UserRegistrationForm()
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, "User registered successfully!")
+            user = form.save(commit=False)  # salva sem ainda salvar no banco
+            # ou 'password' dependendo do form
+            password = form.cleaned_data.get('password')
+            user.set_password(password)  # agora sim, senha criptografada!
+            user.save()
+            messages.success(request, "Usuário registrado com sucesso!")
             return redirect('ecommerce:index')
         else:
-            messages.error(request, "Please correct the errors below.")
+            messages.error(request, "Corrija os erros abaixo.")
     else:
         form = UserRegistrationForm()
-    contex = {'form': form}
-    return render(request, 'create_user.html', contex)
+
+    context = {'form': form}
+    return render(request, 'create_user.html', context)
 
 
 def login_page(request):
@@ -41,8 +45,9 @@ def handle_logout(request):
     logout(request)
     return redirect('authentication:login')
 
+
 @login_required
 def list_users(request):
     users = User.objects.all()
     contex = {'users': users}
-    return render (request, 'list_users.html', contex)
+    return render(request, 'list_users.html', contex)
